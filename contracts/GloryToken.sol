8e1bc509e1a8dc4e8e7e36478ac4f4f7813a3234
Alpha <alpha@gloryfinance.io>
2023-02-07 15:01:04 +0700

    implement the logic for charge fee

--- contracts/GloryToken.sol
+++ contracts/GloryToken.sol
@@ -11,12 +11,17 @@ contract GloryToken is ERC20PresetMinterPauserUpgradeable {
 
     uint256 public constant  MAX_SUPPLY = 150000000 * 10 ** 18;
 
+    uint256 public constant  MAX_SUPPLY_PUBLIC = 135000000 * 10 ** 18;
+
+    uint256 public constant  MAX_SUPPLY_TEAM = 15000000 * 10 ** 18;
+
     uint256 public constant TIME_MINT_TO_TEAM = 1707238111;
 
     bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
 
     bytes32 public constant OPERATOR_TEAM = keccak256("OPERATOR_TEAM");
 
+    mapping(address => bool) private dexes;
 
 
     function initialize()
@@ -26,23 +31,33 @@ contract GloryToken is ERC20PresetMinterPauserUpgradeable {
         __ERC20PresetMinterPauser_init("Glory", "GLR");
     }
 
-    function mint(address to, uint256 amount)  public virtual override {
+    function mint(address to, uint256 amount) public virtual override {
         require(
             hasRole(OPERATOR_ROLE, _msgSender()),
             "must have operator role"
         );
-        require(super.totalSupply() + amount + (15000000*10**18) < MAX_SUPPLY, "Total supply over max supply");
+        require(super.totalSupply() + amount < MAX_SUPPLY, "Total supply over max supply");
         super.mint(to, amount);
     }
 
-    function mintToTeam(address to)  public virtual override {
+    function mintToTeam(address to) public {
         require(
             hasRole(OPERATOR_TEAM, _msgSender()),
             "must have operator role"
         );
         require(block.timestamp >= TIME_MINT_TO_TEAM, "Time mint invalid");
 
-        super.mint(to, 15000000*10**18);
+        mint(to, MAX_SUPPLY_TEAM);
+    }
+
+
+    function _mint(address to, uint256 amount) public {
+        require(
+            hasRole(OPERATOR_ROLE, _msgSender()),
+            "must have operator role"
+        );
+        require(super.totalSupply() + amount < MAX_SUPPLY_PUBLIC, "Total supply over max supply public");
+        mint(to, amount);
     }
 
     function transferFrom(
@@ -50,12 +65,31 @@ contract GloryToken is ERC20PresetMinterPauserUpgradeable {
         address to,
         uint256 amount
     ) public virtual override returns (bool){
-        uint256 balanceOf = super.balanceOf(from);
-        require(amount <= balanceOf, "Balance not enough");
-        uint256 amountTransfer = amount.mul(99).div(100);
-        uint256 amountFee = amount.sub(amountTransfer);
-        super.transfer(address(this), amountFee);
-        return super.transferFrom(from, to, amountTransfer);
+        if (dexes[from]) {
+            uint256 balanceOf = super.balanceOf(from);
+            require(amount <= balanceOf, "Balance not enough");
+            uint256 amountTransfer = amount.mul(99).div(100);
+            uint256 amountFee = amount.sub(amountTransfer);
+            super.transfer(address(this), amountFee);
+            return super.transferFrom(from, to, amountTransfer);
+        } else if (dexes[to]) {
+            uint256 balanceOf = super.balanceOf(from);
+            uint256 amountFee = amount.div(100);
+            require(amount.add(amountFee) <= balanceOf, "Balance not enough");
+            super.transfer(address(this), amountFee);
+            return super.transferFrom(from, to, amount);
+        } else {
+            return super.transferFrom(from, to, amount);
+        }
+    }
+
+
+    function addDexAddress(address dex) external {
+        require(
+            hasRole(OPERATOR_ROLE, _msgSender()),
+            "must have operator role"
+        );
+        dexes[dex] = true;
     }
 
 
